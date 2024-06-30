6688869527f5fc5fa6dc428e577958790e77656f
Hugobsan <hugocorone10@gmail.com>
2024-06-29 22:51:05 -0300

    feat: configurando autenticação

--- hotelize-app/routes/web.php
+++ hotelize-app/routes/web.php
@@ -1,19 +1,25 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CepController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Login
+Route::post('login', [AuthController::class, 'login']);
 
 Route::get('/csrf', function () {
     return response()->json(['csrf' => csrf_token()]);
 });
 
-Route::resource('hotels', HotelController::class)->except('create', 'edit');
-Route::resource('hotels.rooms', RoomController::class)->except('create', 'edit')->shallow();
+Route::group(['middleware' => ['auth']], function () {
+    Route::post('logout', [AuthController::class, 'logout']);
 
-Route::get('api/cep/{cep}', [CepController::class, 'getCepInfo'])->name('cep');
+    Route::resource('hotels', HotelController::class)->except('create', 'edit');
+    Route::resource('hotels.rooms', RoomController::class)->except('create', 'edit')->shallow();
+
+    Route::group(['prefix' => 'api', 'as' => 'api.'], function () {
+        Route::get('cep/{cep}', [CepController::class, 'getCepInfo'])->name('cep');
+    });
+});
